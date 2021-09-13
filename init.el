0740f423d4c2a5f85f9bc3014a3ca6fb9926bcaa
Samuel Hastings <samuelhastings@Samuels-iMac-2.local>
2021-09-13 09:11:43 -0700

    add projectile find command

--- init.el
+++ init.el
@@ -412,6 +412,7 @@
 
 (zmax/leader-keys
   "pp"  'projectile-switch-project
+  "pf"  'projectile-find-file
   "pc"  'projectile-compile-project
   "pd"  'projectile-dired)
 
@@ -837,16 +838,3 @@
 
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(evil-surround yasnippet ws-butler which-key web-mode visual-fill-column use-package undo-fu typescript-mode smartparens rjsx-mode rainbow-delimiters pyvenv python-mode org-bullets no-littering magit lsp-ui lsp-ivy ivy-rich ivy-prescient helpful general flycheck expand-region exec-path-from-shell evil-org evil-nerd-commenter evil-iedit-state evil-escape evil-collection eshell-toggle eshell-git-prompt doom-themes doom-modeline dired-single dired-open dired-hide-dotfiles dashboard dap-mode counsel-projectile company-box command-log-mode auto-package-update all-the-icons-dired add-node-modules-path)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
